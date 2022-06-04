f8953182f3ff44c562a26e76a93bd2f5edd52de4
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-04 15:03:57 -0300

    criado def init com seus atributos e criado função marcarConsulta

---
+++ paciente.py
@@ -0,0 +1,8 @@
+class Paciente():
+    def __init__(self, nome, idade, cpf):
+        self._nome = nome
+        self._idade = idade
+        self._cpf = cpf
+
+    def marcarConsulta(self, Especialidade, Medico, dia, hora):
+        pass
