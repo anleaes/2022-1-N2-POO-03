10b00bb6f88c2b8c49f9b3eb2a7e4de2c104f03a
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-08 15:59:05 -0300

    retirado parametro medico de especialidade colocado parametro especialidade em medico

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from medico import Medico
 from especialidade import Especialidade
 
 paciente1 = Paciente("fabio", 24, 000000000)
-medico1 = Medico("julio", 37, 11111)
-especialidade1 = Especialidade("neurologista", medico1)
+
+medico1 = Medico("julio", 37, 11111, espec1)
+espec1 = Especialidade("neurologista")
