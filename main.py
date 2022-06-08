bde64152c9fa86a6c83bfc49f36173f29b69ca92
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-08 16:29:52 -0300

    correção de ordem de instancia

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from medico import Medico
 from especialidade import Especialidade
 
 paciente1 = Paciente("fabio", 24, 000000000)
-
-medico1 = Medico("julio", 37, 11111, espec1)
 espec1 = Especialidade("neurologista")
+medico1 = Medico("julio", 37, 11111, espec1)
