0abd5d0e37a7ebe397e1645f20c5ef3a1482323e
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 17:45:08 -0300

    excluido metodo marcarConsulta

--- paciente.py
+++ paciente.py
@@ -3,9 +3,3 @@ class Paciente():
         self._nome = nome
         self._idade = idade
         self._cpf = cpf
-
-    def marcarConsulta(self, Especialidade, dia, hora):
-        if dia < 30 and dia > 0:
-            print(f"CONSULTA MARCADA COM: {Especialidade.getNome()} / MARCADO dia {dia}, hora {hora}")
-            pass # fim if
-        pass # fim marcarConsulta
