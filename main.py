7bbf2c824dcfa24037f6ff5a223d9a8a77701946
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 20:32:36 -0300

    instanciado cadastroP1

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ clinica1 = Clinica("feldmann", "rua fulano de tal", 51995563241)
 sala1 = Sala(1, 1, clinica1)
 exame1 = Exames("sangue", "laboritorial")
 paciente1 = Paciente("fabio", 24)
+cadastroP1 = CadastroPaciente(11144477700, "rua ciclano de tal", 123456789, paciente1)
 medico1 = Medico("luiz", 553992, paciente1)
 
 agenda1 = Agenda("12:00", sala1, exame1, medico1)
