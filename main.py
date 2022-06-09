422c182e634e00d515b5fc167695a156da8e7342
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 20:30:03 -0300

    instanciado medico1

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ clinica1 = Clinica("feldmann", "rua fulano de tal", 51995563241)
 sala1 = Sala(1, 1, clinica1)
 exame1 = Exames("sangue", "laboritorial")
 
+medico1 = Medico("luiz", 553992, paciente1)
+
 agenda1 = Agenda("12:00", sala1, exame1, medico1)
