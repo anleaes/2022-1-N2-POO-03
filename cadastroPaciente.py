09008bc9f10c18b888546d9d187d982df58c8879
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 18:01:29 -0300

    metodo def init completado

--- cadastroPaciente.py
+++ cadastroPaciente.py
@@ -1,2 +1,6 @@
 class CadastroPaciente:
     def __init__(self, cpf, endereco, telefone, paciente):
+        self._cpf = cpf
+        self._endereco = endereco
+        self._telefone = telefone
+        self._paciente = paciente
