032102d437226cbe6a5e5199fe210d68b5afa71a
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 20:25:49 -0300

    importado paciente

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from exames import Exames
 from medico import Medico
 from clinica import Clinica
 from especialidade import Especialidade
+from paciente import Paciente
 
 agenda1 = Agenda("12:00", sala1, exame1, medico1)
