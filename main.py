8af2ea1b3e50ff24582be6f4b5b369f75a598ef7
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 20:33:36 -0300

    instanciado especialidade1

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ exame1 = Exames("sangue", "laboritorial")
 paciente1 = Paciente("fabio", 24)
 cadastroP1 = CadastroPaciente(11144477700, "rua ciclano de tal", 123456789, paciente1)
 medico1 = Medico("luiz", 553992, paciente1)
+especialidade1 = Especialidade("neurologista", medico1)
+
+
 
 agenda1 = Agenda("12:00", sala1, exame1, medico1)
