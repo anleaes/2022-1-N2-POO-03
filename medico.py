15134fe73f946b0cda8055898217b81344e10b24
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 18:08:45 -0300

    incluido parametro paciente no init

--- medico.py
+++ medico.py
@@ -1,8 +1,9 @@
 class Medico():
-    def __init__(self, nome, idade, crm):
+    def __init__(self, nome, idade, crm, paciente):
         self._nome = nome
         self._idade = idade
         self._crm = crm
+        self._paciente = paciente
 
     def getNome(self):
         return self._nome
