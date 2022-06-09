8b5930191ec6e1c7c8d819a14fb33a30e2d17259
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 18:48:00 -0300

    criado metodo def init

--- agenda.py
+++ agenda.py
@@ -4,23 +4,3 @@ class Agenda:
         self._sala = sala
         self._exame = exame
 
-    def getHora(self):
-        return self._hora
-    def setHora(self, hora):
-        self._hora = hora
-
-    def getSala(self):
-        return self._sala
-    def setSala(self, sala):
-        self._sala = sala
-
-    def getExame(self):
-        return self._exame
-    def setExame(self, exame):
-        self._exame = exame
-
-    def verificarHorario(self, hora, sala):
-        pass
-
-    def agendarExame(self, exame):
-        pass
