bf9f1b98ee3aeea724267cea905abb10dc30378d
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 19:01:47 -0300

    adicionado parametro medico no init

--- agenda.py
+++ agenda.py
@@ -1,8 +1,9 @@
 class Agenda:
-    def __init__(self, hora, sala, exame):
+    def __init__(self, hora, sala, exame, medico):
         self._hora = hora
         self._sala = sala
         self._exame = exame
+        self._medico = medico
 
     def getHora(self):
         return self._hora
