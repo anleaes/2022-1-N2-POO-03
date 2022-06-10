706e876679f53c5e57038435e40d720bd24b444e
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-09 21:18:22 -0300

    instanciado endereco1

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from clinica import Clinica
 from especialidade import Especialidade
 from paciente import Paciente
 from cadastroPaciente import CadastroPaciente
+from endereco import Endereco
 
+endereco1 = Endereco("rua fulano de tal", 55, "centro", "cidade1")
 clinica1 = Clinica("feldmann", "rua fulano de tal", 51995563241)
 sala1 = Sala(1, 1, clinica1)
 exame1 = Exames("sangue", "laboritorial")
