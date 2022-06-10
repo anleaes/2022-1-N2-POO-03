a02de0ebfe444e20267c388a3ed70d4558f7e842
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-10 18:10:49 -0300

    criado get e set para ddd

--- telefone.py
+++ telefone.py
@@ -2,3 +2,8 @@ class Telefone:
     def __init__(self, ddd, numero):
         self._ddd = ddd
         self._numero = numero
+
+    def getDDD(self):
+        return self._ddd
+    def setDDD(self, ddd):
+        self._ddd = ddd
