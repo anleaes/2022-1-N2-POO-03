b65ec1911a2ea96abfd17ae0b328266410c3f886
iKickBR <fabio.feldmann97@gmail.com>
2022-06-10 20:03:43 -0300

    feita a chamada de verificaCadastro: teste 1

--- main.py
+++ main.py
@@ -24,3 +24,5 @@ especialidade1 = Especialidade("neurologista", medico1)
 
 
 agenda1 = Agenda("12:00", sala1, exame1, medico1)
+
+cadastroP1.verificaCadastro(paciente1, 11144477700, endereco2, telefone2)
