335398a8c572477c6e444f387e8ede9450352b3c
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-06-14 15:57:58 -0300

    separado instancias para ver melhor o codigo

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ from telefone import Telefone
 
 endereco1 = Endereco("rua fulano de tal", 55, "centro", "cidade")
 endereco2 = Endereco("rua ciclano de tal", 247, "barro sul", "cidade")
+
 telefone1 = Telefone(51, 912345678)
 telefone2 = Telefone(51, 987654321)
+
 clinica1 = Clinica("feldmann", endereco1, telefone1)
+
 sala1 = Sala(1, 1, clinica1)
+
 exame1 = Exames("sangue", "laboratorial")
+
 paciente1 = Paciente("fabio", 24)
+
 cadastroP1 = CadastroPaciente(11144477700, endereco2, telefone2, paciente1)
+
 medico1 = Medico("luiz", 553992, paciente1)
+
 especialidade1 = Especialidade("neurologista", medico1)
 
 
@@ -28,4 +36,5 @@ agenda1 = Agenda("12:00", sala1, exame1, medico1)
 cadastroP1.verificaCadastro(paciente1, 11144477700, endereco2, telefone2)
 cadastroP1.verificaCadastro(paciente1, "11144477700", endereco2, telefone1)
 
+agenda1.agendarExame(exame1, medico1, "12:00", sala1)
 agenda1.agendarExame(exame1, medico1, "12:00", sala1)
